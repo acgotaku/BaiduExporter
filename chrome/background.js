150a5a73fbee9ee30aa394296a40f6bff0a234cb
acgotaku <acgotaku311@gmail.com>
2014-08-23 17:58:01 +0800

    release 0.1.6

--- chrome/background.js
+++ chrome/background.js
@@ -1,23 +1,33 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     console.log(tab);
     if (changeInfo.status === 'loading' && tab.url.indexOf("n.baidu.com") != -1) {
-        var domain = "http://pan.baidu.com/";
-        var name = "BDUSS";
-        chrome.cookies.get({"url": domain, "name": name}, function(cookies) {
-            if (cookies) {
-                var data = cookies.name + "=" + cookies.value;
-                console.log(data);
-                chrome.runtime.onMessage.addListener(
-                        function(request, sender, sendResponse) {
-                            if (request.do == "get_cookie") {
-                                sendResponse({cookie: data});
-                            }
+        if (!chrome.runtime.onConnect.hasListeners()) {
+            chrome.runtime.onConnect.addListener(function(port) {
+                console.assert(port.name == "get_cookie");
+                port.onMessage.addListener(function(request) {
+                    console.log(request);
+                    if (request.do == "get_cookie") {
+                        var option = {};
+                        if (request.site) {
+                            option["url"] = request.site;
+                        }
+                        if (request.name != null) {
+                            option["name"] = request.name;
+                        }
+                        chrome.cookies.get(option, function(cookies) {
+                            var obj = {};
+                            obj[cookies.name] = cookies.value;
+                            port.postMessage(obj);
+                            console.log(obj);
                         });
-            }
-        });
+
+                    }
+                });
+            });
+        }
 
     }
 
-});
 
+});
 
