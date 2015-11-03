0fda94b1c087db1ac9e7b19efb5f0baaf09af0a5
acgotaku <acgotaku311@gmail.com>
2015-11-03 21:25:25 +0900

    fix bug

--- chrome/js/baidu.js
+++ chrome/js/baidu.js
@@ -11,22 +11,23 @@ function addJS(name){
     (document.body || document.head || document.documentElement).appendChild(s);
 }
 onload(function() {
-	//把函数注入到页面中
+    //把函数注入到页面中
     var home = window.location.href.indexOf("/disk/home") != -1 ? true : false;
     var album = window.location.href.indexOf("/pcloud/album/info") != -1 ? true : false;
     addJS("core");
     addJS("connect");
     if(home){
         addJS("home");
-        return ;
-    }
-    if(album){
-        addJS("album");
+       
     }else{
-        addJS("share");
-        addJS("convert");
+        if(album){
+            addJS("album");
+        }else{
+            addJS("share");
+            addJS("convert");
+        }    
     }
     window.setTimeout(function(){
-    	window.postMessage(chrome.runtime.id,'*');
+        window.postMessage(chrome.runtime.id,'*');
     },300);
 });
