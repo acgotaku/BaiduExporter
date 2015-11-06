a405ec18cfe6899e3b2bfa4cd0c5d3f44624b783
acgotaku <acgotaku311@gmail.com>
2015-11-06 22:59:28 +0900

    release 0.5.4

--- chrome/background.js
+++ chrome/background.js
@@ -127,7 +127,7 @@ if(previousVersion == "" || previousVersion != manifest.version){
     var opt={
         type: "basic",
         title: "更新",
-        message: "百度网盘助手更新到" +manifest.version + "版本啦～\n此次更新更换UA突破限速请重置设置~",
+        message: "百度网盘助手更新到" +manifest.version + "版本啦～\n此次更新修复BUG~",
         iconUrl: "images/icon.jpg"
     }
     var id= new Date().getTime().toString();              
