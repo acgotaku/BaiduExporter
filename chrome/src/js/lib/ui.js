f5d763bd064b42cb7bb192dbf1f33c978ad6c018
acgotaku <acgotaku311@gmail.com>
2019-04-15 23:26:56 +0900

    update build flow

--- chrome/src/js/lib/ui.js
+++ chrome/src/js/lib/ui.js
@@ -3,8 +3,8 @@ import Store from './store'
 
 class UI {
   constructor () {
-    this.version = '1.0.2'
-    this.updateDate = '2017/12/30'
+    this.version = '1.0.3'
+    this.updateDate = '2019/04/15'
     Store.on('updateView', (configData) => {
       this.updateSetting(configData)
       this.updateMenu(configData)
